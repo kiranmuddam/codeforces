3c9f7a7e1b31cf4a028921c36fc65652f67d522a
Kiran Babu Muddam <kiraniiitn@gmail.com>
2021-07-13 08:12:34 +0530

    Add solution for 1393A

---
+++ 1393/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+#pragma GCC optimize ("O2")
+#pragma GCC optimize ("unroll-loops")
+
+using namespace std;
+void solveProblem() {
+    long long int N;
+    cin >> N;
+    cout<< (N / 2) + 1 <<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testCasesCount;
+    cin >> testCasesCount;
+    while(testCasesCount--) {
+        solveProblem();
+    }
+}
